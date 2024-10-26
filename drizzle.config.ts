8c96f0eaee55dd8776c81ee74fa15e8da189935e
John Neusteter <jneusteter@remtechindustries.com>
2024-10-26 09:56:30 -0400

    add drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'drizzle-kit'
+
+export default defineConfig({
+  dialect: 'sqlite',
+  schema: './server/database/schema.ts',
+  out: './server/database/migrations',
+  dbCredentials: { url: process.env.DB_FILE_NAME! },
+})
