529895783137783f7ec5f3afca5825a66dbf999b
John Neusteter <jneusteter@remtechindustries.com>
2024-10-30 18:50:49 -0400

    add columns

---
+++ server/database/migrations/0004_spotty_tempest.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `batteries` ADD `status` text;--> statement-breakpoint
+ALTER TABLE `batteries` ADD `MWh` integer;--> statement-breakpoint
+ALTER TABLE `batteries` ADD `MW` integer;--> statement-breakpoint
+ALTER TABLE `batteries` ADD `manufacturer` text;--> statement-breakpoint
+ALTER TABLE `batteries` ADD `project_website` text;
